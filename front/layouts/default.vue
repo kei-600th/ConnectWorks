2e47811cbb2af738287d8efea819948a41f488aa
kei-600th <kei19941005@gmail.com>
2023-05-05 17:03:14 +0900

    ドロップダウンメニューの作成

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -3,11 +3,33 @@
     <div>
       <v-app-bar app color="deep-purple-accent-4" dense dark clipped-left>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>ConnectWorks</v-toolbar-title>
 
-        <v-btn>MyProject</v-btn>
+        
+
+          <v-menu offset-y nudge-bottom="16px">
+            <template #activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                プロジェクト
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in items"
+                :key="index"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+
 
         <v-spacer></v-spacer>
-
         <v-btn icon>
           <v-icon>mdi mdi-account-circle</v-icon>
         </v-btn>
@@ -33,3 +55,16 @@
     </div>
   </v-app>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+    }),
+  }
+</script>
