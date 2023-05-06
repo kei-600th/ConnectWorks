d5d105bb48685a6d4d7014736cc258c508f69a81
kei-600th <kei19941005@gmail.com>
2023-05-06 15:14:05 +0900

    projectsテーブルの作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_06_060206) do
+ActiveRecord::Schema.define(version: 2023_05_06_061130) do
 
   create_table "projects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
