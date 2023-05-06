4d7706733a71f762642bf12660ccf87a5c5ad964
kei-600th <kei19941005@gmail.com>
2023-05-06 15:25:02 +0900

    projectコントローラーの作成

---
+++ api/app/controllers/v1/projects_controller.rb
@@ -0,0 +1,26 @@
+class V1::ProjectsController < ApplicationController
+
+  def index
+    project = Project.all
+    render json: project
+  end
+
+  def create
+    puts params
+    project = Project.new(project_params)
+    if project.save
+      render json: project
+    else
+      render json: project.errors
+    end
+  end
+
+    # ストロングパラメーターの設定
+    def project_params
+      params.require(:project).permit(:name,)
+    end
+
+
+
+
+end
