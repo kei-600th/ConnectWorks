e282a6cda35e81cdca0d21cc7ee59ba01937ba26
kei-600th <kei19941005@gmail.com>
2023-05-08 19:37:09 +0900

    AppBarをdefaultに戻す

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <AppBar />
     <TaskList :tasks="tasks" />
   </div>
 </template>
 <script>
-import AppBar from '@/components/AppBar'
 import TaskList from '@/components/TaskList'
 export default {
   components: {
-    AppBar,
     TaskList,
   },
   data() {
