61642c501de506d3c825c79e705a6ad8d5a9a238
kei-600th <kei19941005@gmail.com>
2023-05-10 18:35:57 +0900

    task.validを行う場合にはcreateではなくbuildを使用する

--- api/spec/models/task_spec.rb
+++ api/spec/models/task_spec.rb
@@ -7,24 +7,23 @@ RSpec.describe Task, type: :model do
     # .buildだと関連するプロジェクトが作られないので.createを使ってください
     it 'is valid with a name, status, and project_id' do
       task = FactoryBot.create(:task)
-      binding.pry
       expect(task).to be_valid
     end
     
     it 'is invalid without a name' do
-      task = FactoryBot.create(:task, name: nil)
+      task = FactoryBot.build(:task, name: nil)
       task.valid?
       expect(task.errors[:name]).to include("can't be blank")
     end
 
     it 'is invalid without a status' do
-      task = FactoryBot.create(:task, status: nil)
+      task = FactoryBot.build(:task, status: nil)
       task.valid?
       expect(task.errors[:status]).to include("can't be blank")
     end
 
     it 'is invalid without a project_id' do
-      task = FactoryBot.create(:task, project: nil)
+      task = FactoryBot.build(:task, project: nil)
       task.valid?
       expect(task.errors[:project]).to include("must exist")
     end
