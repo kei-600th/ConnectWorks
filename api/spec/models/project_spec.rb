285d4887dd9e19dfc2ad79c960fff5e7c1c75063
kei-600th <kei19941005@gmail.com>
2023-05-14 16:12:05 +0900

    モデルテストにコメント文を追加

--- api/spec/models/project_spec.rb
+++ api/spec/models/project_spec.rb
@@ -1,15 +1,16 @@
 # spec/models/project_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   describe 'validation' do
-    # ここにバリデーションのテストを記述
-
+    # プロジェクトが有効であることを検証するテストケース
     it 'is valid with a name' do
       project = FactoryBot.build(:project)
       expect(project).to be_valid
     end
 
+    # 名前がない場合にプロジェクトが無効であることを検証するテストケース
     it 'is invalid without a name' do
       project = FactoryBot.build(:project, name: nil)
       project.valid?
@@ -22,6 +23,7 @@ RSpec.describe Project, type: :model do
     let!(:task1) { FactoryBot.create(:task, project: project) }
     let!(:task2) { FactoryBot.create(:task, project: project) }
 
+    # プロジェクトが削除されたときに関連するタスクも削除されることを検証するテストケース
     it 'deletes associated tasks when destroyed' do
       expect { project.destroy }.to change { Task.count }.by(-2)
     end
--- api/spec/models/task_spec.rb
+++ api/spec/models/task_spec.rb
@@ -1,26 +1,31 @@
 # spec/models/task_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'validation' do
-    # .buildだと関連するプロジェクトが作られないので.createを使ってください
+    # タスクが有効であることを検証するテストケース
+    # ※ .buildだと関連するプロジェクトが作られないので.createを使ってください
     it 'is valid with a name, status, and project_id' do
       task = FactoryBot.create(:task)
       expect(task).to be_valid
     end
 
+    # 名前がない場合にタスクが無効であることを検証するテストケース
     it 'is invalid without a name' do
       task = FactoryBot.build(:task, name: nil)
       task.valid?
       expect(task.errors[:name]).to include("can't be blank")
     end
 
+    # ステータスがない場合にタスクが無効であることを検証するテストケース
     it 'is invalid without a status' do
       task = FactoryBot.build(:task, status: nil)
       task.valid?
       expect(task.errors[:status]).to include("can't be blank")
     end
 
+    # プロジェクトIDがない場合にタスクが無効であることを検証するテストケース
     it 'is invalid without a project_id' do
       task = FactoryBot.build(:task, project: nil)
       task.valid?
